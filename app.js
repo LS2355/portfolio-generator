1bdaf114f1b9e5dfca4fe244ed9df99a124024fc
Luis S <Luissalcedo2355@hotmail.com>
2022-09-21 04:23:57 -0700

    asked a question with inquirer

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const fs = require('fs');
-//import
-const generatePage = require('./src/page_template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(name, github);
 
-const [name, Github] = profileDataArgs;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', generatePage(name, Github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Checkout index.html to see the output!')
-});
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+        }
+    ])
+.then(answers => console.log(answers));
