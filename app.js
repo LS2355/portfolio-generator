20c1d6031974fc46effbb5c3d1d1d4d58806e645
Luis S <Luissalcedo2355@hotmail.com>
2022-09-26 02:30:45 -0700

    final functionality

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const fs = require('fs');
+const generateSite = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page_template.js');
+const {writeFile, copyFile } = require('./src/page_template.js');
 
-// const pageHTML = generatePage(name, github);
+const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 const promptUser = () => {
     return inquirer
@@ -156,14 +152,21 @@ portfolioData.projects = [];
     });
   };
  
-  promptUser()
+promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML)
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
